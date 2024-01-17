12c7477116c12157bf17753a42991dfa1489fb39
Konstantin Kovalev <constantiner@gmail.com>
2024-01-17 14:03:22 +0100

    feat: Add lint-staged and husky

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./app.tsx";
+import { App } from "./application.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
