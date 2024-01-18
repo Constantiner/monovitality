d594979d56db0aa4bce8f4d1fb40146b3059bdc6
Konstantin Kovalev <constantiner@gmail.com>
2024-01-18 15:48:45 +0100

    feat: Resolve local font files in Vite build
    feat: Import using @use rule in SCSS files

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,6 +1,8 @@
 import svgr from "@svgr/rollup";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { resolve, dirname, sep } from "node:path";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line no-restricted-syntax
@@ -12,6 +14,21 @@ export default defineConfig({
 				// this is required for the SCSS modules
 				find: /^~(.*)$/,
 				replacement: "$1"
+			},
+			{
+				// this is required for the fonts
+				find: /^(.*)\.woff2$/,
+				replacement: `${resolve(dirname(fileURLToPath(import.meta.url)), "../components/src/styles/fonts/")}${sep}$1.woff2`
+			},
+			{
+				// this is required for the fonts
+				find: /^(.*)\.woff$/,
+				replacement: `${resolve(dirname(fileURLToPath(import.meta.url)), "../components/src/styles/fonts/")}${sep}$1.woff`
+			},
+			{
+				// this is required for the fonts
+				find: /^(.*)\.ttf$/,
+				replacement: `${resolve(dirname(fileURLToPath(import.meta.url)), "../components/src/styles/fonts/")}${sep}$1.ttf`
 			}
 		]
 	}
