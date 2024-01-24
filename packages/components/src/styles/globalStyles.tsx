60dd387e8767fb12a614769fcef6ec9d6bc33c62
Konstantin Kovalev <constantiner@gmail.com>
2024-01-24 14:20:49 +0100

    feat: Dark color theme support

--- packages/components/src/styles/globalStyles.tsx
+++ packages/components/src/styles/globalStyles.tsx
@@ -1,9 +1,14 @@
-import type { EmptyType } from "@monovitality/types";
 import type { FunctionComponent, PropsWithChildren } from "react";
 import "./index.scss";
 
-export const GlobalStyles: FunctionComponent<PropsWithChildren<EmptyType>> = ({
-	children
-}: PropsWithChildren<EmptyType>): JSX.Element => {
+type GlobalStylesProperties = { className?: string };
+
+export const GlobalStyles: FunctionComponent<PropsWithChildren<GlobalStylesProperties>> = ({
+	children,
+	className
+}): JSX.Element => {
+	if (className) {
+		return <div className={className}>{children}</div>;
+	}
 	return <>{children}</>;
 };
--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -1,8 +1,11 @@
 import { Button, GlobalStyles, IconHolder, getSvgIcon } from "@monovitality/components";
+import classNames from "clsx";
 import { Suspense, lazy, useEffect, useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import "./app.scss";
 import { ReactComponent as ReactLogo } from "./react.svg";
 import { ReactComponent as ViteLogo } from "./vite.svg";
+import { useMediaQuery } from "@react-hook/media-query";
 
 const SubmoduleApp = lazy(() =>
 	import("@monovitality/submodule").then(module => ({
@@ -18,8 +21,14 @@ const viteLogoIcon = getSvgIcon(ViteLogo);
 const defaultCounter = (count: number): number => count;
 
 function App(): JSX.Element {
+	const [isDarkTheme, setIsDarkTheme] = useState<boolean>(useMediaQuery("(prefers-color-scheme: dark)"));
 	const [count, setCount] = useState(0);
 	const [counter, setCounter] = useState<Counter>(defaultCounter);
+	useHotkeys("alt+t", () => setIsDarkTheme(previousIsDarkTheme => !previousIsDarkTheme));
+
+	const cssClass = classNames({
+		"theme-dark": isDarkTheme
+	});
 
 	useEffect(() => {
 		async function loadCounter(): Promise<void> {
@@ -30,29 +39,32 @@ function App(): JSX.Element {
 	}, []);
 
 	return (
-		<>
-			<GlobalStyles />
-			<div className="logo-container">
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<IconHolder icon={viteLogoIcon} className="logo" title="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<IconHolder icon={reactLogoIcon} className="logo react" title="React Logo" />
-				</a>
-			</div>
-			<p>{import.meta.env.VITE_TEST_KEY}</p>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<Button onClick={() => setCount(counter)}>count is {count}</Button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-			<Suspense fallback="Loading footer…">
-				<SubmoduleApp />
-			</Suspense>
-		</>
+		<div className={cssClass}>
+			<GlobalStyles className="monovitality-web-base-container">
+				<main className="monovitality-content">
+					<div className="logo-container">
+						<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+							<IconHolder icon={viteLogoIcon} className="logo" title="Vite logo" />
+						</a>
+						<a href="https://react.dev" target="_blank" rel="noreferrer">
+							<IconHolder icon={reactLogoIcon} className="logo react" title="React Logo" />
+						</a>
+					</div>
+					<p>{import.meta.env.VITE_TEST_KEY}</p>
+					<h1>Vite + React</h1>
+					<div className="card">
+						<Button onClick={() => setCount(counter)}>count is {count}</Button>
+						<p>
+							Edit <code>src/App.tsx</code> and save to test HMR
+						</p>
+					</div>
+					<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+				</main>
+				<Suspense fallback="Loading footer…">
+					<SubmoduleApp />
+				</Suspense>
+			</GlobalStyles>
+		</div>
 	);
 }
 
