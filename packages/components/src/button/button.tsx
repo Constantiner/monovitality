bea9c2d33238dc3eff36b51c8fe6e3c1269ea918
Konstantin Kovalev <constantiner@gmail.com>
2024-02-13 11:22:59 +0100

    feat: Add button variants and sizes

--- packages/components/src/button/button.tsx
+++ packages/components/src/button/button.tsx
@@ -1,17 +1,61 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import cn from "clsx";
 import { forwardRef, type ButtonHTMLAttributes } from "react";
-import { P, match } from "ts-pattern";
+import { match, P } from "ts-pattern";
+import { IconHolder } from "../iconHolder/iconHolder";
+import type { IconProperties } from "../iconHolder/iconHolderUtil";
 import "./button.scss";
 
-export type ButtonProperties = ButtonHTMLAttributes<HTMLButtonElement> & { label?: string };
+const buttonVariants = cva("monovitality-button", {
+	variants: {
+		variant: {
+			default: "monovitality-button--default",
+			secondary: "monovitality-button--secondary",
+			outline: "monovitality-button--outline"
+		},
+		size: {
+			default: "monovitality-button--default-size",
+			sm: "monovitality-button--sm",
+			lg: "monovitality-button--lg",
+			icon: "monovitality-button--icon"
+		}
+	},
+	defaultVariants: {
+		variant: "default",
+		size: "default"
+	}
+});
+
+export type ButtonProperties = ButtonHTMLAttributes<HTMLButtonElement> &
+	VariantProps<typeof buttonVariants> & { label?: string; icon?: IconProperties };
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProperties>((properties, reference) => {
-	const { label, ...rest } = properties;
+	const { label, icon, variant, size, className, ...rest } = properties;
 	return match(label)
-		.with(P.nullish, () => <button className="monovitality-button" ref={reference} {...rest} />)
-		.otherwise(label => (
-			<button className="monovitality-button" ref={reference} {...rest}>
-				{label}
-			</button>
-		));
+		.with(P.nullish, () =>
+			match(icon)
+				.with(P.nullish, () => (
+					<button className={cn(buttonVariants({ variant, size, className }))} ref={reference} {...rest} />
+				))
+				.otherwise(icon => (
+					<button className={cn(buttonVariants({ variant, size, className }))} ref={reference} {...rest}>
+						<IconHolder icon={icon}></IconHolder>
+					</button>
+				))
+		)
+		.otherwise(label =>
+			match(icon)
+				.with(P.nullish, () => (
+					<button className={cn(buttonVariants({ variant, size, className }))} ref={reference} {...rest}>
+						{label}
+					</button>
+				))
+				.otherwise(icon => (
+					<button className={cn(buttonVariants({ variant, size, className }))} ref={reference} {...rest}>
+						<IconHolder icon={icon}></IconHolder>
+						{label}
+					</button>
+				))
+		);
 });
 Button.displayName = "Button";
--- packages/storybook/src/components/button.stories.ts
+++ packages/storybook/src/components/button.stories.ts
@@ -1,20 +1,212 @@
-import { Button } from "@monovitality/components";
+import { faFaceSmile as faButtonIcon } from "@fortawesome/free-solid-svg-icons";
+import { Button, getFontAwesomeIcon } from "@monovitality/components";
 import type { Meta, StoryObj } from "@storybook/react";
+
+const buttonIcon = getFontAwesomeIcon(faButtonIcon);
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof Button> = {
 	title: "Monovitality/Button",
 	component: Button,
 	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-	tags: ["autodocs"]
+	tags: ["autodocs"],
+	argTypes: {
+		label: {
+			control: "text",
+			description: "The label of the button"
+		},
+		variant: {
+			control: "select",
+			options: ["default", "secondary", "outline", undefined],
+			defaultValue: "default",
+			description: "The variant of the button"
+		},
+		size: {
+			control: "select",
+			options: ["default", "sm", "lg", "icon", undefined],
+			defaultValue: "default",
+			description: "The size of the button"
+		},
+		icon: {
+			control: false,
+			description: "The icon of the button"
+		},
+		disabled: {
+			control: "boolean",
+			defaultValue: false,
+			description: "Whether the button is disabled"
+		},
+		onClick: {
+			action: "clicked",
+			description: "The action to be executed when the button is clicked"
+		}
+	}
 };
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
 	args: {
-		label: "Button"
+		label: "Default Button"
+	}
+};
+
+export const DefaultWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		icon: buttonIcon
+	}
+};
+
+export const DefaultSmall: Story = {
+	args: {
+		label: "Default Button",
+		size: "sm"
+	}
+};
+
+export const DefaultSmallWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		size: "sm",
+		icon: buttonIcon
+	}
+};
+
+export const DefaultLarge: Story = {
+	args: {
+		label: "Default Button",
+		size: "lg"
+	}
+};
+
+export const DefaultLargeWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		size: "lg",
+		icon: buttonIcon
+	}
+};
+
+export const DefaultIcon: Story = {
+	args: {
+		size: "icon",
+		icon: buttonIcon
+	}
+};
+
+export const Secondary: Story = {
+	args: {
+		label: "Secondary Button",
+		variant: "secondary"
+	}
+};
+
+export const SecondaryWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		variant: "secondary",
+		icon: buttonIcon
+	}
+};
+
+export const SecondarySmall: Story = {
+	args: {
+		label: "Default Button",
+		variant: "secondary",
+		size: "sm"
+	}
+};
+
+export const SecondarySmallWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		variant: "secondary",
+		size: "sm",
+		icon: buttonIcon
+	}
+};
+
+export const SecondaryLarge: Story = {
+	args: {
+		label: "Default Button",
+		variant: "secondary",
+		size: "lg"
+	}
+};
+
+export const SecondaryLargeWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		variant: "secondary",
+		size: "lg",
+		icon: buttonIcon
+	}
+};
+
+export const SecondaryIcon: Story = {
+	args: {
+		variant: "secondary",
+		size: "icon",
+		icon: buttonIcon
+	}
+};
+
+export const Outline: Story = {
+	args: {
+		label: "Outline Button",
+		variant: "outline"
+	}
+};
+
+export const OutlineWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		variant: "outline",
+		icon: buttonIcon
+	}
+};
+
+export const OutlineSmall: Story = {
+	args: {
+		label: "Default Button",
+		variant: "outline",
+		size: "sm"
+	}
+};
+
+export const OutlineSmallWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		variant: "outline",
+		size: "sm",
+		icon: buttonIcon
+	}
+};
+
+export const OutlineLarge: Story = {
+	args: {
+		label: "Default Button",
+		variant: "outline",
+		size: "lg"
+	}
+};
+
+export const OutlineLargeWithIcon: Story = {
+	args: {
+		label: "Default Button",
+		variant: "outline",
+		size: "lg",
+		icon: buttonIcon
+	}
+};
+
+export const OutlineIcon: Story = {
+	args: {
+		variant: "outline",
+		size: "icon",
+		icon: buttonIcon
 	}
 };
