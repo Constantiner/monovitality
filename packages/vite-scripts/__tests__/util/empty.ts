fda1227917db32e2864b48aa5c309e65dc00dfb8
Konstantin Kovalev <constantiner@gmail.com>
2024-02-20 16:27:27 +0100

    chore: Update dependencies

--- packages/vite-scripts/__tests__/util/empty.ts
+++
@@ -1,4 +0,0 @@
-export interface EmptyType {}
-
-export const isEmptyType = (value: unknown): value is EmptyType =>
-	value !== undefined && value !== null && typeof value === "object" && Object.keys(value).length === 0;
