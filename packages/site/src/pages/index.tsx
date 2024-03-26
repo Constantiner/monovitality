96f07e52f99cfb8903817a7887980160c582a2a0
Konstantin Kovalev <constantiner@gmail.com>
2024-03-26 16:56:45 +0100

    feat(site): Add dark mode support

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -1,15 +1,60 @@
 import { Button, GlobalStyles } from "@monovitality/components";
+import { useMediaQuery } from "@react-hook/media-query";
 import type { HeadFC, PageProps } from "gatsby";
 import { graphql } from "gatsby";
-import { type FunctionComponent } from "react";
+import { useEffect, useState, type FunctionComponent } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 import { GithubLink } from "../components/githubLink";
 import "./index.scss";
+
+const hasClass = (element: Element, className: string): boolean => {
+	if (element.classList) {
+		return element.classList.contains(className);
+	}
+	return new RegExp("(\\s|^)" + className + "(\\s|$)").test(element.className);
+};
+
+const addClass = (element: Element | null, className: string): void => {
+	if (!element) {
+		return;
+	}
+	if (element.classList) {
+		element.classList.add(className);
+	} else if (!hasClass(element, className)) {
+		element.className += " " + className;
+	}
+};
+
+const removeClass = (element: Element | null, className: string): void => {
+	if (!element) {
+		return;
+	}
+	if (element.classList) {
+		element.classList.remove(className);
+	} else if (hasClass(element, className)) {
+		const reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
+		element.className = element.className.replace(reg, " ");
+	}
+};
 
 type IndexPageProperties = PageProps<Queries.IndexPageQuery>;
 
 const IndexPage: FunctionComponent<IndexPageProperties> = ({ data }) => {
+	const [isDarkTheme, setIsDarkTheme] = useState<boolean>(useMediaQuery("(prefers-color-scheme: dark)"));
+	useHotkeys("alt+t", () => setIsDarkTheme(previousIsDarkTheme => !previousIsDarkTheme));
 	const { allMarkdownRemark } = data;
 	const { edges } = allMarkdownRemark;
+
+	useEffect(() => {
+		const mainElement = document.querySelector("body");
+		if (mainElement) {
+			if (isDarkTheme) {
+				addClass(mainElement, "theme-dark");
+			} else {
+				removeClass(mainElement, "theme-dark");
+			}
+		}
+	}, [isDarkTheme]);
 
 	if (edges.length === 0 || !edges[0].node.html) {
 		return (
