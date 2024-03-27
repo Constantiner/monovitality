10fb801e40cb134b39209c8a85c20ae7ef64e1c4
Konstantin Kovalev <constantiner@gmail.com>
2024-03-27 11:26:05 +0100

    feat(site): Add deploymen Github action

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -9,6 +9,7 @@ const config: GatsbyConfig = {
 	// If you use VSCode you can also use the GraphQL plugin
 	// Learn more at: https://gatsby.dev/graphql-typegen
 	graphqlTypegen: true,
+	pathPrefix: "/monovitality",
 	plugins: [
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-image`,
