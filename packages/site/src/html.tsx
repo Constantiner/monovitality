bd1045bd9e4b9813646f392425dd398f907e56f1
Konstantin Kovalev <constantiner@gmail.com>
2024-04-03 11:59:40 +0200

    fix(site): Fix dark mode flickering issue

--- packages/site/src/html.tsx
+++ packages/site/src/html.tsx
@@ -22,6 +22,13 @@ export default function HTML(properties: HTMLProperties): JSX.Element {
 				{properties.headComponents}
 			</head>
 			<body {...properties.bodyAttributes}>
+				<script
+					key="monovitality-theme"
+					dangerouslySetInnerHTML={{
+						__html: `(function() { try { var isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches; if (isDarkMode)
+  document.body.classList.add("theme-dark"); } catch (e) {} })();`
+					}}
+				/>
 				{properties.preBodyComponents}
 				<div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: properties.body }} />
 				{properties.postBodyComponents}
